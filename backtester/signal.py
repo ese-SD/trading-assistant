80324008d11b262d9f9453d221050d474efae11a
Simon Decostanzi <gbsny8zh@gmail.com>
2025-09-25 23:58:18 +0200

    created the signal class.

---
+++ backtester/signal.py
@@ -0,0 +1,33 @@
+class Signal():
+    """
+    This class contains the informations necessary to place and then fulfill an order.
+
+    It allows a Strategy to place various typs of orders, which will get picked up by Brain to be verified and then fulfilled.
+    
+    Attributes:
+        id (int): id of the signal/order.
+        ttype (str): type of signal.
+        ticker (str): the ticker on which the order is placed.
+        share_nb (float): the number of shares intended to be bought.
+        money (float): amount of money sent in the order.
+        
+    """
+    def __init__(self, id, ttype, ticker, share_nb,money):
+        """
+        Args:
+        id (int): used by Brain for the historic of orders amongs other things.
+        ttype (str): the type of the order, on which depends how it will be handled by Brain.
+        ticker (str): the ticker on which the order is placed.
+        share_nb (float): the number of share to buy (or *want* to buy, in case of market orders, because of slippage)
+        money (float): the actual amount in money of the order. This also determines how much shares are bought for BUY_MARKET orders(because slippage).
+        
+        """
+        types=["BUY_MARKET", "BUY_LIMIT", "SELL_MARKET", "SELL_LIMIT"]
+        if (type(id)==int and ttype in types and type(ticker)==str and type(share_nb)==float):
+            self.id=id
+            self.type=type
+            self.ticker=ticker
+            self.share_nb=share_nb
+            self.money=money
+        else:
+            raise ValueError("Invalid arguments.")
